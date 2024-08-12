a56d797c1461149d5dbe475f4bd68f73cc279766
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-13 08:32:11 +0900

    튜플, 객체, 진위값에 대해 실습

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -10,3 +10,22 @@ let num: number = 10;
 let arr:Array<number> = [1,2,3,];
 let heroes: Array<string> = ['Capt', 'Thor', 'hulk'];
 let items: number[] = [1,2,3,];
+
+// 튜플
+let address: string[] = ['강남', '판교'];
+let address2: [string, number] = ['강남', 100];
+
+//TS 객체
+let obj: object = {};
+// let person: object = [{
+//     name: 'capt',
+//     age: 100
+// }
+
+let person2: { name2: string, age2: number} = {
+    name2: 'thor',
+    age2: 1000
+}
+
+// ts 진위값
+let show: boolean = true;
