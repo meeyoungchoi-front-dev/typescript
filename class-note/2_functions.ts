2bca698d4fbbe7163ee94e2147dcdc06e20aaed0
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-13 08:44:25 +0900

    함수의 타입을 정의하는 방법 실습

---
+++ class-note/2_functions.ts
@@ -0,0 +1,17 @@
+// 함수 파라미터의 타입을 정의하는 박ㅇ식
+function sum(a: number,b: number) {
+    return a + b;
+}
+
+sum(10, 20);
+
+// 함수의 반환 값의 타입을 정의
+function add(): number {
+    return 10;
+}
+
+
+// 함수 타입을 정의하는 방식
+function sum2(a: number, b: number) : number {
+    return a + b;
+}
