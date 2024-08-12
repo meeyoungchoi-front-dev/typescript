92f6d946967240bddd6348768c80da2efe4f79aa
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-13 08:12:47 +0900

    타입스크립트 string, number, array 타입 실습

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,12 @@
+// JS
+let str = 'hello';
+
+// TS 
+let str2: string = 'hello';
+const str3: string = 'hi';
+
+let num: number = 10;
+
+let arr:Array<number> = [1,2,3,];
+let heroes: Array<string> = ['Capt', 'Thor', 'hulk'];
+let items: number[] = [1,2,3,];
