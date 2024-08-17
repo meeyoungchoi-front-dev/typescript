7542696d34f2607a024ef36ef1ecc67fb91ad955
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-17 16:49:44 +0900

    함수에 인터페이스 활용

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -3,7 +3,19 @@ interface User {
     name: string;
 }
 
+// 변수에 인터페이스 활용
 let seho: User = {
     age: 33,
     name: '세호'
 }
+
+// 함수에 인터페이스 활용
+function getUser(user: User) {
+    console.log(user);
+}
+const capt =  {
+    age: 100,
+    name: '캡틴'
+
+}
+getUser(capt);
