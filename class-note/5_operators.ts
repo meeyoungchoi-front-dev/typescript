fc1b590ab982bd5bc6858d143c96b0d6914b7b8a
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-19 09:18:17 +0900

    인터렉션 타입이란

--- class-note/5_operators.ts
+++ class-note/5_operators.ts
@@ -39,8 +39,15 @@ interface Person {
 function askSomeone(someone: Developer  | Person) {
    //  실제적으로 Developer와 Person이 공통으로 갖고 있는 속성만 접근할 수 있게 된다
    // 타입 검증없이 skill이나 age를 쓰면 에러가 발생될 수 있다
-   if typeof someone === 'Developer' ) {
-        someone.
-   }
+   // 타입가드에 대한 추가 적인 처리ㅎ가 필요하다
+}
+
+let sahoow : string | number | boolean;
+let capton : string & number & boolean; // 인터게션 타입 :  3개의 타입을 모두 만복해야 한다
+
+function askSomeone2(someone: Developer & Person) { // Developer가 갖고 있는 name과 skill 그리고 Person이 갖고 있는 모든 name과 age를 포함한 3개의 속성을 갖는 타입
+    someone.age
+    someone.name
+    someone.skill
 
 }
