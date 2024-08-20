1b8450ec823f7efc9e4fb093d483aaca5a03c4cc
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-20 12:58:07 +0900

    문자형 이넘

--- class-note/6_enum.js
+++ class-note/6_enum.js
@@ -1,9 +1,9 @@
 var Shoes;
 (function (Shoes) {
-    Shoes[Shoes["Nike"] = 0] = "Nike";
-    Shoes[Shoes["Adidas"] = 1] = "Adidas";
+    Shoes["Nike"] = "\uB098\uC774\uD0A4";
+    Shoes["Adidas"] = "\uC544\uB514\uB2E4\uC2A4";
 })(Shoes || (Shoes = {}));
 var myShoes = Shoes.Nike;
-console.log(myShoes); // 0 (별도의 값을 지정하지 않으면 전부다 숫자인 0으로 취급하게 된다)
+console.log(myShoes);
 var myShoes2 = Shoes.Adidas;
-console.log(myShoes2); // 1 (별도의 값을 지정하지 않으면 전부다 숫자인 1로 취급하게 된다)
+console.log(myShoes2);
