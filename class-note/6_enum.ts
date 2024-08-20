f7b316c3b7943f669e008b6ff968a5049eebf819
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-20 13:07:20 +0900

    이넘 활용 사례

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -8,3 +8,22 @@ console.log(myShoes);  // 나이키
 
 let myShoes2 = Shoes.Adidas;
 console.log(myShoes2); // 아디다스
+
+enum Answer {
+    Yes = 'Y',
+    No = 'N',
+}
+
+
+
+function askQuestion(answer: Answer) {
+    if (Answer.Yes === answer) {
+        console.log('정답입니다');
+    }
+
+    if (Answer.No  === answer) {
+        console.log('오답입니다');
+    }
+}
+
+askQuestion(Answer.Yes);
