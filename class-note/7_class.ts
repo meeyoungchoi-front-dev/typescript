a59433ff381f1b5773c961d6e4cc0082a33d317b
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-21 12:15:53 +0900

    타입스크립트에서의 클래스 문법

---
+++ class-note/7_class.ts
@@ -0,0 +1,10 @@
+class Person3 {
+    private name3: string;
+    public age3: number;
+    readonly log: string;
+
+    constructor(name3: string, age3: number) {
+        this.name3 = name3;
+        this.age3 = age3;
+    }
+}
