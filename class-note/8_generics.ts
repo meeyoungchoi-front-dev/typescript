fd871c0598e0cd9c42272eb326d2bb015e932f11
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-22 12:22:39 +0900

    제네릭을 사용하지 않았을때 단점

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -17,3 +17,21 @@ logText2<number>(10); // 숫자 10
 logText2<string>('하이'); // 문자열 하이
 logText2<boolean>(true); // 진위값 true
 
+function logText3(text:string) {
+    console.log(text);
+    text.split('').reverse().join('');
+    return text;
+}
+
+function logNumber(text:number) {
+    console.log(text);
+    return text;
+}
+
+function logBoolean(text: boolean) {
+    console.log(Boolean);
+    return text;
+}
+logNumber(10); // 숫자 10
+logText3('하이'); // 문자열 하이
+logBoolean(true); // 진위값 true
