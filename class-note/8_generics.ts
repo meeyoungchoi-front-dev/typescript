8a1f0d259386fd41e920e4a951528a2f3a659f0d
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-23 16:02:16 +0900

    keyOf 예약어를 사용해서 제네릭타입 제한

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -94,3 +94,17 @@ function logTextLength2<T extends LengthType>(text: T): T {
 
 logTextLength2('a');
 logTextLength2({length: 10})
+
+
+// 제네릭 타입 제한 3 - keyof
+interface ShoppingItem {
+    name: string;
+    price: number;
+    stock: number;
+}
+
+function getShoppingItemOption<T extends keyof ShoppingItem >(itemOption: T): T {
+    return itemOption;
+}
+
+getShoppingItemOption("price");
